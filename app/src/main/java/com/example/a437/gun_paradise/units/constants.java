dbaa46de57fc48f87262b0ca5ab55ac4db128882
Unicorn437 <creak@arcor.de>
2016-06-17 18:30:40 +0200

    static constants

--- app/src/main/java/com/example/a437/gun_paradise/units/constants.java
+++ app/src/main/java/com/example/a437/gun_paradise/units/constants.java
@@ -3,13 +3,14 @@ package com.example.a437.gun_paradise.units;
 /**
  * Created by 437 on 15-Jun-16.
  */
-public class constants {
+public class Constants {
     //----------------------------------------------------------------Noob
-    public final double noob_sBasedmg = 1;
-    public final double noob_sAttackrate = 1;
-    public final double noob_increment = 0.1;
-    public final double noob_sPrice = 5;
-    public final int noob_id = 1;
+    public static final double noob_sBasedmg = 1;
+    public static final double noob_sAttackrate = 1;
+    public static final double noob_increment = 0.1;
+    public static final double noob_sPrice = 5;
+    public static final int noob_id = 1;
+    public static final int camper_id = 1;
     //----------------------------------------------------------------
 
 }
