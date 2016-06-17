e6840fc6c33c16e2943816bbb473721cf8292065
Unicorn437 <creak@arcor.de>
2016-06-17 18:14:56 +0200

    fixed errors

--- app/src/main/java/com/example/a437/gun_paradise/themes/Theme.java
+++ app/src/main/java/com/example/a437/gun_paradise/themes/Theme.java
@@ -3,6 +3,6 @@ package com.example.a437.gun_paradise.themes;
 /**
  * Created by 437 on 17-Jun-16.
  */
-public Enum Theme {
-    city, hills, rainbow, desert
-}
+    public enum Theme {
+        city, hills, rainbow, desert
+    }
